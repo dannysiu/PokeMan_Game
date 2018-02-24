4acf22a4685af8faffa5e98ba0395a3a3580a143
Ben Sydserff <bsydserff@Bens-MacBook-Pro.local>
2018-02-23 17:30:53 -0800

    Protecting my hallways

--- proj2/byog/Core/HallwayGenerator.java
+++ proj2/byog/Core/HallwayGenerator.java
@@ -2,6 +2,7 @@ package byog.Core;
 
 import byog.Core.WorldGenerator;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
 
 /** A class to handle hallway generation.
  *  Assumes that rooms are already created and randomly dispersed in world. Will look for
@@ -17,30 +18,30 @@ public class HallwayGenerator {
     /** Creates a hallway to connect two unlocked doors. Calls horizontal, vertical,
      *  and corner contructors as necessary.
      */
-    public HallwayGenerator(position one, position two) { // Position class not compiling?
+    public HallwayGenerator(position one, position two, TETile[][] world) { // Position class not compiling?
         // TODO: Algorithim to select what hallways needed to connect two positions.
 
     }
 
     /** Creates a horizonatal hallway. */
-    private class horizontalHallway {
-        int length;
-        int height = 3;
+    private void horizontalHallway(int length, int horizPos, int vertPos, TETile[][] world) {
 
-        private horizontalHallway(int length, int horizPos, int vertPos) {
-
-            TETile[][] hallway = new TETile[horizPos + length][height];
-
+        for (int i = horizPos; i < (horizPos + length); i += 1) {
+            world[i][vertPos] = Tileset.FLOOR;
+            world[i][vertPos - 1] = Tileset.WALL;
+            world[i][vertPos + 1] = Tileset.WALL;
         }
-
-
     }
 
+
     /** Creates a vertical hallway. */
-    private class verticalHallway {
+    private void verticalHallway(int height, int horizPos, int vertPos, TETile[][] world) {
 
-
-
+        for (int i = vertPos; i < (vertPos + height); i += 1) {
+            world[horizPos][i] = Tileset.FLOOR;
+            world[horizPos - 1][i] = Tileset.WALL;
+            world[horizPos + 1][i] = Tileset.WALL;
+        }
     }
 
     /** Creates an L corner connecting hallways. */
