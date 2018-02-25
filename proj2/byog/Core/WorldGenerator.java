3f87e093c73e1daca042d8d6c73b4b7771025e7f
Ben Sydserff <bsydserff@Bens-MacBook-Pro.local>
2018-02-25 15:41:07 -0800

    Adding changes to unlockedFinder method

--- proj2/byog/Core/WorldGenerator.java
+++ proj2/byog/Core/WorldGenerator.java
@@ -2,31 +2,34 @@ package byog.Core;
 
 import byog.TileEngine.TETile;
 import static byog.TileEngine.Tileset.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorldGenerator {
 
 
-    /** Creates an array of all unlocked doors in the world (signifying places where rooms need
+    /** Creates a List of all unlocked doors in the world (signifying places where rooms need
      *  to be connected via hallways).
-     *  TODO: replace array of Positions with a linkedlist.
      *  TODO: find a way to replace 80 and 30 ints with dynamic world size.
      */
-    private Position[] unlockedFinder(TETile[][] world) {
-        Position[] allUnlocked = new Position[1000]; // Arbitrarily long array
-        int index = 0;
+    private List<Position> unlockedFinder(TETile[][] world) {
+        List<Position> allUnlocked = new ArrayList<>();
+//        int index = 0;
+//        int xPos;
+//        int yPos;
 
-        for (int x; x < 80; x += 1) { // Using 80 for width and 30 for height because of default values
-            for (int y; y < 30; y += 1) { // that Game.java uses to make a world
-                if (world[x][y].equals(UNLOCKED_DOOR)) {
-                    Position p = new Position(x, y);
-                    allUnlocked[index] = p;
-                    index += 1; // Index only increments as UNLOCKED_DOOR positions added
+        for (int xPos = 0; xPos < 80; xPos += 1) { // Using 80 for width and 30 for height because of default values
+            for (int yPos = 0; yPos < 30; yPos += 1) { // that Game.java uses to make a world
+                if (world[xPos][yPos].equals(UNLOCKED_DOOR)) {
+                    Position p = new Position(xPos, yPos);
+                    allUnlocked.add(p);
+//                    index += 1; // Index only increments as UNLOCKED_DOOR positions added
                 }
             }
         }
-        Position[] skimmedAllUnlocked = new Position[index];
-        System.arraycopy(allUnlocked, 0, skimmedAllUnlocked, 0, index);
-        return skimmedAllUnlocked; // Return a shorter array of just the desired Positions.
+//        Position[] skimmedAllUnlocked = new Position[index];
+//        System.arraycopy(allUnlocked, 0, skimmedAllUnlocked, 0, index);
+        return allUnlocked; // Return a shorter array of just the desired Positions.
     }
 
 }
