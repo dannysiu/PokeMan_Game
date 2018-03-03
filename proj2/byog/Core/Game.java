0d704236f63ad4fafb457b4c772693fe5ba00aec
Danny Siu <ds46429@berkeley.edu>
2018-03-03 00:34:41 -0800

    We got ourselves a menugit status :)

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -17,36 +17,70 @@ public class Game {
     public static final int HEIGHT = 40;
 
 
+
+
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
     public void playWithKeyboard() {
 
         TETile[][] world = new TETile[WIDTH][HEIGHT];
+        drawMenu();
 
-        StdDraw.clear(StdDraw.BLACK);
-        StdDraw.setCanvasSize(WIDTH * 8, HEIGHT* 8);
-        Font font = new Font("Monaco", Font.BOLD, 35);
-        StdDraw.setFont(font);
-        StdDraw.setXscale(0, WIDTH);
-        StdDraw.setYscale(0, HEIGHT);
-        StdDraw.text(WIDTH / 2, HEIGHT / 2, "CS 61B");
-        StdDraw.show();
+        String typing = "";
+        while (typing.length() < 50000000) {  //only exit the program
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            char input = StdDraw.nextKeyTyped();
+            if (input == 'q' || input == 'Q') {
+                System.exit(0);
+                break;
+            } else if (input == 'n' || input == 'N') {
+                /** TODO: Pop out a window to get the seed,
+                 * if valid, then call Ben's code for playing the Game using the seed
+                 * I think we should put his code in the playNewGame method in this class
+                 * */
+
+                //make window
+                //call playNewGame(); if valid seed
 
 
 
-//        StdDraw.drawFrame("Round " + round + "! Good luck!");
-//        StdDraw.pause(1500);
-//
-//
-//        input =
+            } else if (input == 'l' || input == 'L') {
+                /** TODO: Load the previous game that was terminated
+                 * and its random seed*/
+            } else {
+                Font font = new Font("Monaco", Font.PLAIN, 12);
+                StdDraw.setFont(font);
+                StdDraw.text(WIDTH / 2, HEIGHT / 2, "Not a valid command");
+                StdDraw.show();
+            }
+            typing += input;
+        }
+    }
+
+
+
+//        while (input.length() < n) {
+//            if (!StdDraw.hasNextKeyTyped()) {
+//                continue;
+//            }
+//            char key = StdDraw.nextKeyTyped();
+//            input += String.valueOf(key);
+//            drawFrame(input);
+//        }
+
+
+
+
 //
 //        if (input.startsWith("N") || input.startsWith("n")) {
 //            playWithInputString(input);
 //
 //
 //        }
-    }
+
 
 
 
@@ -102,7 +136,41 @@ public class Game {
         }
 
         return world;
+    }
 
+
+    public void drawMenu() {
+        StdDraw.setCanvasSize(WIDTH * 16, HEIGHT* 16);
+        StdDraw.clear(StdDraw.BOOK_LIGHT_BLUE);
+
+
+        Font font1 = new Font("Helvetica", Font.BOLD, 45);
+        Font font2 = new Font("Monaco", Font.BOLD, 30);
+        Font font3 = new Font("Monaco", Font.PLAIN, 18);
+        StdDraw.setXscale(0, WIDTH);
+        StdDraw.setYscale(0, HEIGHT);
+
+        StdDraw.setFont(font1);
+        StdDraw.text(WIDTH / 2, 3 * HEIGHT / 4, "Pika-Man");
+        StdDraw.setFont(font2);
+        StdDraw.text(WIDTH / 2, 2 * HEIGHT / 3, "CS 61B Project 2");
+        StdDraw.setFont(font3);
+        StdDraw.text(WIDTH / 2, 7 * HEIGHT / 12, "Created by Ben Sydserff & Danny Siu");
+
+
+        StdDraw.text(WIDTH / 2, 6 * HEIGHT / 17, "New Game (N)");
+        StdDraw.text(WIDTH / 2, 5 * HEIGHT / 16, "Load Game (L)");
+        StdDraw.text(WIDTH / 2, 4 * HEIGHT / 16, "Quit (Q)");
+
+
+        StdDraw.show();
 
     }
+
+
+    public void playNewGame() {
+        // insert Ben's code for playing New Game
+    }
+
+
 }
